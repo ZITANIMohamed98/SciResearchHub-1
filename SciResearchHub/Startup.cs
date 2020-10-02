ffc85887f7b44a98f6cb6cbd238ed262cbdd9c5c
ZITANIMohamed98 <60824720+ZITANIMohamed98@users.noreply.github.com>
2020-10-02 18:22:10 +0100

    fix razor service

--- SciResearchHub/Startup.cs
+++ SciResearchHub/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SciResearchHub.Services;
 
 namespace SciResearchHub
 {
@@ -24,6 +25,7 @@ namespace SciResearchHub
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddTransient<ResearchExtractService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
