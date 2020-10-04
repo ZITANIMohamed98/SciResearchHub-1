ce3709ca86e8847b1fa9256afb29c3495639cd76
Awsomebuddy <72265806+Awsomebuddy@users.noreply.github.com>
2020-10-04 14:11:23 +0100

    Update site.js

--- SciResearchHub/wwwroot/js/site.js
+++ SciResearchHub/wwwroot/js/site.js
@@ -1,5 +1,17 @@
-﻿let collapsebutton[] = document.querySelectorAll(".collapse")
-.addEventListener('click', function (e) {
-    e.target.collapse();
-});
+let collapsebutton = document.querySelectorAll(".category")
+let t = 0;
+for (let i = 0; i < collapsebutton.length; i++) {
+    collapsebutton[i].addEventListener('click', function (e) {
+        if (t%2===0) {
+            e.target.classList.add("active")
+        }
+        else {
+            for (let i = 0; i < collapsebutton.length; i++) {
+                collapsebutton[i].classList.remove("active")
+            }
+        }
+        t++;
+    });
+    
+}
 
